2738f269e144d34a82035f531c9f19c9ad3765be
jjunnnn0 <46149307+jjunnnn0@users.noreply.github.com>
2018-12-27 01:59:25 +0900

    change to utf-8

--- 2292.c
+++ 2292.c
@@ -7,7 +7,7 @@ int main(void) {
 	int sum = 1, n = 0;
 	while(1){
 		n++;
-		sum += (n - 1)*d; // ´©Àû? µîÂ÷¼ö¿­
+		sum += (n - 1)*d; // 누적 등차수열?
 		if (N <= sum) {
 			printf("%d\n", n);
 			return 0;
